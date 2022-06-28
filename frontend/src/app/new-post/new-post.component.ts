315e97e5ded6d61c547a6b063dd137002a18e81a
Alexandre <meurotalexandre@yahoo.fr>
2022-06-28 11:24:15 +0200

    création formulaire d'ajout de nouveau post

--- frontend/src/app/new-post/new-post.component.ts
+++ frontend/src/app/new-post/new-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-new-post',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPostComponent implements OnInit {
 
-  constructor() { }
+  // variable contenant le formulaire
+  postForm!: FormGroup;
+
+  // injection de l'outil FormBuilder = création de formulaire réactif
+  constructor(private formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
+
+    this.postForm = this.formBuilder.group({
+      title: [null],
+      imageUrl: [null],
+      description: [null],
+      location: [null]
+    })
+
+  }
+
+  // méthode d'envoie du formulaire - prévoir envoie au serveur
+  onSubmitForm(): void {
+    console.log(this.postForm.value);
   }
 
 }
