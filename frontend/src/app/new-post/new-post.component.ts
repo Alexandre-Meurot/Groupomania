b9cf03e7dffe651763aed61c3c0b3273004bd090
Alexandre <meurotalexandre@yahoo.fr>
2022-06-28 12:16:17 +0200

    Ajout de Validators sur les champs d'ajout d'un post + regex pour l'email

--- frontend/src/app/new-post/new-post.component.ts
+++ frontend/src/app/new-post/new-post.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {map, Observable} from "rxjs";
 import {Post} from "../models/post.model";
 
@@ -10,22 +10,31 @@ import {Post} from "../models/post.model";
 })
 export class NewPostComponent implements OnInit {
 
-  // variable contenant le formulaire
+  // variable contenant le formulaire de type FormGroup
   postForm!: FormGroup;
-
   // variable d'observable = emmission de type Post
   postPreview$!: Observable<Post>;
+  // variable pattern pour validator de l'email de type RegExp
+  urlRegex!: RegExp;
 
   // injection de l'outil FormBuilder = création de formulaire réactif
   constructor(private formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
 
+    // initialisation de urlRegex
+    this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
+
+    // définition des champs du formulaire
     this.postForm = this.formBuilder.group({
-      title: [null],
-      imageUrl: [null],
-      description: [null],
+      title: [null, Validators.required],
+      imageUrl: [null, [Validators.required, Validators.pattern(this.urlRegex)]],
+      description: [null, Validators.required],
       location: [null]
+    },
+    {
+      // formulaire mis à jour que lorsque l'on change de champs
+      updateOn: 'blur'
     });
 
     // observable qui emmet tous les changement du formulaire postForm
