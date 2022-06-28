2bbedc7f3c8345760bb2505c0e2ab9575d4c877d
Alexandre <meurotalexandre@yahoo.fr>
2022-06-28 11:00:11 +0200

    creation méthode d'ajout d'un nouveau post

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  // injection du routeur
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  // méthode d'ajout d'un nouveau post - redirection vers le composant new-post
+  onAddNewPost(): void {
+    this.router.navigateByUrl('/create')
   }
 
 }
