fe24b00f9f1cf5048415a614e9641614a60c73d6
Alexandre <meurotalexandre@yahoo.fr>
2022-06-28 15:09:30 +0200

    modification méthode likePostbyId

--- frontend/src/app/services/posts.service.ts
+++ frontend/src/app/services/posts.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
 import {Post} from "../models/post.model";
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {map, Observable, switchMap} from "rxjs";
 
 @Injectable({
   providedIn: "root"
@@ -12,9 +12,6 @@ export class PostsService {
   // injection du httpClient
   constructor(private http: HttpClient) {
   }
-
-  // Récupération des posts sous la forme d'un tableau
-  posts: Post[] = [];
 
   // méthode de récupération de tous les posts
   getAllPosts(): Observable<Post[]> {
@@ -27,20 +24,25 @@ export class PostsService {
   }
 
   // méthode de like de post en fonction de son ID
-  likePostById(postId: number, likeType: 'like' | 'disLike'): void {
-    // const post = this.getPostById(postId)
-    // likeType === 'like' ? post.likes++ : post.likes--
+  likePostById(postId: number, likeType: 'like' | 'disLike'): Observable<Post> {
+    return this.getPostById(postId).pipe(
+      map(post => ({
+        ...post,
+        likes: post.likes + likeType === 'like' ? 1 : -1
+      })),
+      switchMap(updatedPost => this.http.put<Post>(`http://localhost:3000/posts/${postId}`, updatedPost))
+    );
   }
 
   // méthode d'ajout du nouveau post avec un ID valable
-  addPost(formValue: { title: string, description: string, imageUrl: string, location: string }): void {
-    const post: Post = {
-      ...formValue,
-      createdDate: new Date(),
-      likes: 0,
-      id: this.posts[this.posts.length - 1].id + 1
-    };
-    this.posts.push(post);
-  }
+  // addPost(formValue: { title: string, description: string, imageUrl: string, location: string }): void {
+  //   const post: Post = {
+  //     ...formValue,
+  //     createdDate: new Date(),
+  //     likes: 0,
+  //     id: this.posts[this.posts.length - 1].id + 1
+  //   };
+  //   this.posts.push(post);
+  // }
 
 }
