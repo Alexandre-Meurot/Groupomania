e1d41d952b0bb60eacef42855c445f21d3c3d91a
Alexandre <meurotalexandre@yahoo.fr>
2022-06-29 14:38:21 +0200

    Mise en place des guards ssur les routes de post

--- frontend/src/app/posts/posts-routing.module.ts
+++ frontend/src/app/posts/posts-routing.module.ts
@@ -3,11 +3,12 @@ import {RouterModule, Routes} from "@angular/router";
 import {NewPostComponent} from "./components/new-post/new-post.component";
 import {SinglePostComponent} from "./components/single-post/single-post.component";
 import {PostListComponent} from "./components/post-list/post-list.component";
+import {AuthGuard} from "../core/guards/auth.guard";
 
 const routes: Routes = [
-  { path: 'create', component: NewPostComponent },
-  { path: ':id', component: SinglePostComponent },
-  { path: '', component: PostListComponent },
+  { path: 'create', component: NewPostComponent, canActivate: [AuthGuard] },
+  { path: ':id', component: SinglePostComponent, canActivate: [AuthGuard] },
+  { path: '', component: PostListComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
