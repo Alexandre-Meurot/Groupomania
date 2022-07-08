a0c6f610b99f56b69d5d0e745b6a7203e52e487c
Alexandre Meurot <meurotalexandre@yahoo.fr>
2022-07-08 18:03:25 +0200

    Correction de post.components

--- frontend/src/app/posts/components/post/post.component.ts
+++ frontend/src/app/posts/components/post/post.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import {Post} from "../../../core/models/post.model";
 import {PostsService} from "../../../core/services/posts.service";
 import {Router} from "@angular/router";
-import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-post',
@@ -12,7 +11,6 @@ import {Observable, tap} from "rxjs";
 export class PostComponent implements OnInit {
   @Input() post!: Post;
   buttonText!: string;
-  posts$!: Observable<Post>;
 
   constructor(private postService: PostsService,
               private router: Router) {}
@@ -25,16 +23,4 @@ export class PostComponent implements OnInit {
   onViewPost() {
     this.router.navigateByUrl(`posts/${this.post.id}`)
   }
-
-  onLike(postId: number) {
-    if (this.buttonText === 'like') {
-      this.posts$ = this.postService.likePostById(postId, "like").pipe(
-        tap(() => this.buttonText = 'disLike')
-      );
-    } else {
-      this.posts$ = this.postService.likePostById(postId, 'disLike').pipe(
-        tap(() => this.buttonText = 'like')
-      );
-    }
-  };
 }
