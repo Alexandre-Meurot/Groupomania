5b832b80c5039c13e4411f80ba4cbc565a1cec27
Alexandre Meurot <meurotalexandre@yahoo.fr>
2022-07-08 15:41:28 +0200

    Ajout de caractéristiques du post dans le fil d'actualité

--- frontend/src/app/posts/components/post/post.component.ts
+++ frontend/src/app/posts/components/post/post.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import {Post} from "../../../core/models/post.model";
 import {PostsService} from "../../../core/services/posts.service";
 import {Router} from "@angular/router";
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-post',
@@ -11,6 +12,7 @@ import {Router} from "@angular/router";
 export class PostComponent implements OnInit {
   @Input() post!: Post;
   buttonText!: string;
+  posts$!: Observable<Post>;
 
   constructor(private postService: PostsService,
               private router: Router) {}
@@ -23,4 +25,16 @@ export class PostComponent implements OnInit {
   onViewPost() {
     this.router.navigateByUrl(`posts/${this.post.id}`)
   }
+
+  onLike(postId: number) {
+    if (this.buttonText === 'like') {
+      this.posts$ = this.postService.likePostById(postId, "like").pipe(
+        tap(() => this.buttonText = 'disLike')
+      );
+    } else {
+      this.posts$ = this.postService.likePostById(postId, 'disLike').pipe(
+        tap(() => this.buttonText = 'like')
+      );
+    }
+  };
 }
