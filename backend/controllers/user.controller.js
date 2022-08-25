ce648c0e3dbed95e40fee3b2d71866a9ab1ede49
Alexandre Meurot <meurotalexandre@yahoo.fr>
2022-08-25 16:23:51 +0200

    Refactorisation méthodes CRUD user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const passwordValidator = require('password-validator')
@@ -17,119 +18,169 @@ schema
 
 // ---------- CREATION D'UN UTILISATEUR -----------
 
-exports.signup = async (req, res) => {
-    try {
-        const userMail = await User.findOne({
-            where: { email: req.body.email }
-        })
-        const userName = await User.findOne({
-            where: { username: req.body.username }
-        })
-        if (userMail !== null) {
-            const message = 'Un compte existe déjà avec cette adresse email !'
-            return res.status(400).json({ message, error })
-        }
-        if (userName !== null) {
-            const message = "Un compte existe déjà avec ce nom d'utilisateur"
-            return res.status(400).json({ message, error })
-        }
-        if (!schema.validate(req.body.password)) {
-            const message = "Le mot de passe doit faire entre 6 et 15 caractères, contenir au moins 1 majuscule, 1 minuscule, 1 chiffre et ne doit pas contenir d'espace !"
-            return res.status(400).json({ message, error })
-        }
-        const usernameRegex = /^([A-Za-z]{3,20})?([-]{0,1})?([A-Za-z\s]{3,20})$/;
-        const mailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+exports.signup =  (req, res) => {
 
-        if (usernameRegex.test(req.body.username) && mailRegex.test(req.body.email)) {
-            const hash = await bcrypt.hash(req.body.password, 10)
-            const newUser = await User.create({
-                username: req.body.username,
-                email: req.body.email,
-                password: hash
-            })
-            const message = `Votre compte a bien été créé ${newUser.username} !`
-            res.status(201).send({
-                user: newUser,
-                message : message
-            })
-        } else {
-            const message = 'Votre Pseudo et/ou votre Email sont incorrect !'
-            res.status(400).json({ message, error })
-        }
-    } catch (error) {
-        const message = `Une erreur est survenue`
-        return res.status(400).json({ message, error })
+    let username = req.body.username
+    let email = req.body.email
+    let password = req.body.password
+
+    const usernameRegex = /^([A-Za-z]{3,20})?([-]{0,1})?([A-Za-z\s]{3,20})$/;
+    const mailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+    if (email == null || email === '' || username == null || username === '' || password == null || password === '') {
+        const message = 'Tous les champs doivent être renseignés (username, email, password)'
+        return res.status(400).json({ message: message, error })
     }
+
+    if (!usernameRegex.test(username)) {
+        const message = 'Votre pseudo ne respecte pas les règles de validation !'
+        return res.status(400).json({ message: message, error })
+    }
+
+    if (!mailRegex.test(email)) {
+        const message = 'Votre email ne respecte pas les règles de validation !'
+        return res.status(400).json({ message: message, error })
+    }
+
+    if (!schema.validate(password)) {
+        const message = 'Le mot de passe doit faire entre 6 et 15 caractères, contenir au moins 1 majuscule, 1 minuscule, 1 chiffre et ne doit pas contenir d\'espace !'
+        return res.status(400).json({ message: message, error })
+    }
+
+    User.findOne({
+        attributes: ['username' || 'email'],
+        where: {
+            username: username,
+            email: email
+        }
+    })
+        .then(userFound => {
+            if (!userFound) {
+                bcrypt.hash(password, 10)
+                    .then(hash => {
+                        const user = User.build({
+                            username: username,
+                            email: email,
+                            password: hash
+                        })
+                        user.save()
+                            .then(() => {
+                                const message = 'Votre compte a bien été créé !'
+                                res.status(201).json({ message: message })
+                            })
+                            .catch(error => {
+                                const message = 'Une erreur est survenue lors de la création de votre compte !'
+                                res.status(400).json({ message: message, error })
+                            })
+                    })
+                    .catch(error => {
+                        const message = 'Une erreur est survenue lors de la création de votre compte !'
+                        res.status(500).json({ message: message, error })
+                    })
+            } else {
+                const message = 'Cet utilisateur existe déjà !'
+                return res.status(404).json({ message: message, error })
+            }
+        })
+        .catch(error => {
+            const message = "Une erreur s'est produite !"
+            res.status(500).json({ message: message, error })
+        })
 }
 
 // ---------- CONNEXION D'UN UTILISATEUR -----------
 
-exports.login = async (req, res) => {
-    try {
-        const user = await User.findOne({
-            where: { email: req.body.email }
-        })
-        if (user === null) {
-            const message = 'Aucun compte ne correspond à cet email !'
-            return res.status(401).json({ message, error })
-        } else {
-            const hash = await bcrypt.compare(req.body.password, user.password)
-            if (!hash) {
-                const message = 'Le mot de passe est incorrect !'
-                return res.status(401).json({ message, error })
+exports.login = (req, res) => {
+
+    User.findOne({
+        where: { email: req.body.email }
+    })
+        .then(userFound => {
+            if (userFound) {
+                bcrypt.compare(req.body.password, userFound.password)
+                    .then(passwordIsValid => {
+                        if (!passwordIsValid) {
+                            const message = 'Mot de passe incorrect !'
+                            return res.status(401).json({ message: message, error })
+                        }
+                        res.status(200).json({
+                            userId: userFound.id,
+                            username: userFound.username,
+                            picture: userFound.picture,
+                            isAdmin: userFound.isAdmin,
+                            token: jwt.sign(
+                                { userId: userFound.id },
+                                 process.env.JWT_SECRET_TOKEN,
+                                { expiresIn: '24h' }
+                            )
+                        })
+                    })
+                    .catch(error => {
+                        const message = 'Une erreur est survenue !'
+                        res.status(500).json({ message: message, error })
+                    })
             } else {
-                const message = `Vous êtes bien connecté(e) ${user.username} :)`
-                res.status(200).send({
-                    user: user,
-                    message: message,
-                    token: jwt.sign(
-                        {userId: user.id},
-                        process.env.JWT_SECRET_TOKEN,
-                        {expiresIn: '24h'}
-                    )
-                })
+                const message = 'Cet email ne correspond à aucun utilisateur, veuillez créer un compte !'
+                res.status(404).json({ message: message, error })
             }
-        }
-    } catch (error) {
-        const message = 'Une erreur est survenue !'
-        return res.status(500).json({ message, error })
-    }
+        })
+        .catch(error => {
+            const message = 'Une erreur est survenue !'
+            res.status(404).json({ message: message, error })
+        })
 }
 
 // ---------- RECUPERATION D'UN UTILISATEUR -----------
 
-exports.getOneUser = async (req, res) => {
-    try {
-        const user = await User.findOne({
-            where: { id: req.params.id }
+exports.getOneUser = (req, res) => {
+
+    const id = req.params.id
+
+    User.findOne({
+        attributes: [ 'id', 'username', 'email', 'picture', 'isAdmin' ],
+        where: { id: id }
+    })
+
+        .then(userFound => {
+            if (userFound) {
+                const message = 'Utilisateur trouvé !'
+                res.status(200).json({ message: message, data: userFound })
+            } else {
+                const message = 'Utilisateur non trouvé !'
+                res.status(404).json({ message: message, error })
+            }
         })
-        res.status(200).send({
-            user: user,
-            message: 'Le profil a bien été trouvé !'
+        .catch(error => {
+            const message = 'Une erreur est survenue !'
+            res.status(404).json({ message: message, error })
         })
-    } catch (error) {
-        const message = 'Une erreur est survenue !'
-        return res.status(500).json({ message, error })
-    }
 }
 
 // ---------- MODIFICATON D'UN UTILISATEUR -----------
 
-exports.updateUser = async (req, res) => {
-    const id = req.params.id;
+exports.updateUser = (req, res) => {
+
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_TOKEN);
+    const userId = decodedToken.userId;
+
+    req.body.user = userId
+
+    console.log('bodyUser', req.body.user);
 
     const userObject = req.file ?
-        { ...JSON.parse(req.body.user),
-            picture: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        {
+            ...JSON.parse(req.body.user),
+            imageProfile: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
         } : { ...req.body };
 
     User.findOne({
-        where: { id: id },
+        where: { id: userId },
     })
         .then(userFound => {
             if(userFound) {
                 User.update(userObject, {
-                    where: { id: id }
+                    where: { id: userId}
                 })
                     .then(user => {
                         const message = 'Votre profil a bien été modifié !'
@@ -139,9 +190,8 @@ exports.updateUser = async (req, res) => {
                         const message = 'Une erreur est survenue !'
                         res.status(400).json({ message: message, error  })
                     })
-            }
-            else {
-                const message = 'Utilisateur non trouvé'
+            } else {
+                const message = 'Utilisateur non trouvé !'
                 res.status(404).json({ message: message, error });
             }
         })
