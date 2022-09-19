7d25b552150a5fd1691532210938fae5030fbd23
Alexandre Meurot <alexandre.meurot@tessi.fr>
2022-09-19 17:03:24 +0200

    frontend : modification méthode de création d'un post

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {LoginComponent} from "./components/auth/login/login.component";
 import {AuthGuard} from "./guards/auth.guard";
 import {HomeComponent} from "./components/home/home.component";
 import {AccountComponent} from "./components/account/account.component";
+import {AddPostComponent} from "./components/add-post/add-post.component";
 
 const routes: Routes = [
-  { path: '', pathMatch: "full", component: LoginComponent },
   { path: 'authentification', component: LoginComponent },
-  { path: 'home', canActivate: [AuthGuard], component: HomeComponent },
   { path: 'account', canActivate: [AuthGuard], component: AccountComponent },
-  { path: '**', redirectTo: '' }
+  { path: 'home', canActivate: [AuthGuard], component: HomeComponent },
+  { path: 'create', canActivate: [AuthGuard], component: AddPostComponent },
+  { path: '', pathMatch: "full", component: LoginComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
--- frontend/src/app/components/add-post/add-post.component.ts
+++ frontend/src/app/components/add-post/add-post.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Post} from "../../models/post.model";
 import {PostService} from "../../services/post.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {map, Observable} from "rxjs";
 
 @Component({
   selector: 'app-add-post',
@@ -11,25 +12,36 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export class AddPostComponent implements OnInit {
 
   newPost!: Post;
-  addPostForm!: FormGroup;
+  postForm!: FormGroup;
+  postPreview$!: Observable<Post>;
+  urlRegex!: RegExp;
 
   constructor(private postService: PostService,
               private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
 
-    this.addPostForm = this.formBuilder.group({
-      content: ['', Validators.required],
-      media: ['']
-    })
+    this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
 
-    this.newPost = new Post()
+    this.postForm = this.formBuilder.group({
+      content: [null, Validators.required],
+      media: [null, Validators.pattern(this.urlRegex)],
+    }, {
+      updateOn: 'blur'
+    });
+
+    this.postPreview$ = this.postForm.valueChanges.pipe(
+      map(formValue => ({
+        ...formValue,
+      }))
+    )
+
   }
 
-  onAddPost() {
-    console.log(this.addPostForm.value)
-    this.postService.createPost(this.addPostForm.value)
-      .subscribe()
+  onAddPost(): void {
+    console.log(this.postForm.value)
+    // this.postService.createPost(this.addPostForm.value)
+    //   .subscribe()
   }
 
 }
