1af1b80a348ef5af9c1763eb05b7800fdf40c5ba
Alexandre Meurot <alexandre.meurot@tessi.fr>
2022-10-06 15:52:53 +0200

    frontend : ajout méthodes modification et suppression de compte via account-detail + styles

--- frontend/src/app/components/account-detail/account-detail.component.ts
+++ frontend/src/app/components/account-detail/account-detail.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {UserService} from "../../services/user.service";
 import {User} from "../../models/user.model";
-import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-account-detail',
@@ -12,10 +11,12 @@ import {Observable} from "rxjs";
 export class AccountDetailComponent implements OnInit {
 
   userId!: string
+  currentUserId!: string | null
   user!: User
 
   constructor(private activated: ActivatedRoute,
-              private userService: UserService) { }
+              private userService: UserService,
+              private router: Router) { }
 
   ngOnInit(): void {
 
@@ -24,10 +25,33 @@ export class AccountDetailComponent implements OnInit {
         this.userId = params['id']
         console.log( this.userId)
       })
-
     this.userService.getUserById(+this.userId)
       .subscribe(user => this.user = user)
 
+    this.currentUserId = localStorage.getItem('userId')
+
   }
 
+  myOwnAccount() {
+    return this.currentUserId == this.userId
+  }
+
+  isAdmin(): boolean {
+    return localStorage.getItem('isAdmin') == 'true';
+  }
+
+  toUpdate() {
+    this.router.navigate(['account'])
+  }
+
+  toDelete() {
+    this.userService.deleteUser(+this.userId)
+      .subscribe(() => {
+        this.router.navigate(['home'])
+      })
+  }
+
+  toHome() {
+    this.router.navigate(['home'])
+  }
 }
